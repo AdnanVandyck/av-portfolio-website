799318c9cfd75ba80fc29b80412b65ff03a68d93
Adnan <adnanvandyck@gmail.com>
2024-09-07 06:28:46 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import About from './components/About';
+import Home from './components/Home';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+
+function App() {
+  return (
+    <Router>
+          <div className="App">
+            <Header />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/projects" element={<Projects />} />
+              <Route path="/contact" element={<Contact />} />
+            </Routes>
+            <Footer />
+          </div>
+    </Router>
+
+  );
+}
+
+export default App;
